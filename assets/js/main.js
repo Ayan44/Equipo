514c03fcba74a9b2534c88c5fa3d411b9ce73fe9
Ayan44 <ayanpharly17@gmail.com>
2024-07-22 01:13:34 +0400

    FAQs-updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -143,3 +143,30 @@ class Accordion {
 document.querySelectorAll("details").forEach((el) => {
   new Accordion(el);
 });
+
+
+
+
+
+
+
+
+
+      var iconSwiper = new Swiper(".my-swiper", {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        breakpoints: {
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+          },
+        },
+      });
